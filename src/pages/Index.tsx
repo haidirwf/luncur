51d8a626e074a94053dc0ecf544415c338f1c370
haidirwf <i914900h@gmail.com>
2026-01-03 07:33:05 +0700

    d protofolio

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -17,7 +17,6 @@ const Index = () => {
       <main>
         <HeroSection />
         <LayananSection />
-        <PortfolioSection />
         <PricelistSection />
         <AboutSection />
         <ContactSection />
